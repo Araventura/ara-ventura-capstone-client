383b7acad123508232b4f144a0ae542a1eb985b0
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-17 12:08:47 -0700

    add icons to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Footer.scss";
+import FacebookIcon from "../../assets/icon/facebook-icon.svg";
+import InstaIcon from "../../assets/icon/instagram-icon.svg";
+import LinkedinIcon from "../../assets/icon/linkedin-icon.svg";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <h4 className="footer__subtitle">Ara Ventura © DentaShift 2024</h4>
+      <div className="fotter__icon-wrapper">
+        <img className="footer__icon" src={FacebookIcon} alt="" />
+        <img className="footer__icon" src={InstaIcon} alt="" />
+        <img className="footer__icon" src={LinkedinIcon} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;

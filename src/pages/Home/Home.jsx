9db4777b15e7ac235e962510eaf8c7179c6d0b65
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-16 20:09:58 -0700

    add home skeleton

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <div>DentaShift</div>;
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="hero__left"></div>
+        <div className="hero__right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

4db4bf04cf76c58b24b6ea4cae58cfff88cedb65
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-16 17:41:27 -0700

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.scss";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import JobsList from "./components/JobsList/JobsList";
+import JobDetails from "./components/JobDetails/JobDetails";
+import Professionals from "./components/ProfessionalsList/ProfessionalsList";
+import ProfessionalDetails from "./components/ProfessionalDetails/ProfessionalDetails";
+import Offices from "./components/OfficesList/OfficesList";
+import OfficeDetails from "./components/OfficeDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <div id="app__body">
         <Header />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<JobsList />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
+          <Route path="/professionals" element={<Professionals />} />
+          <Route path="/professionals/id" element={<ProfessionalDetails />} />
+          <Route path="/offices" element={<Offices />} />
+          <Route path="/offices/id" element={<OfficeDetails />} />
+        </Routes>
         <Footer />
       </div>
     </BrowserRouter>

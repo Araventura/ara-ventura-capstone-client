e3bc5ffd4aae78aeeb298c2aea97bdddbc4a7434
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-16 18:33:51 -0700

    add font Lexend, favicon, logo and two buttons on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Header.scss";
+import Logo from "../../assets/logo/DentaShift.png";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__logo-wrapper">
+        <img src={Logo} alt="DentaShift Logo" />
+      </div>
+      <div className="header__buttons">
+        <Link className="header__button-office btn btn-outline-primary">
+          <button>Dental Office</button>
+        </Link>
+        <Link className="header__button-professional">
+          <button>Dental Professional</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;

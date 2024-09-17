58edcc1d6d17b458ec6ce3a12cb7e6fb9dc002bb
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-16 23:31:49 -0700

    add cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import HeroImage from "../../assets/images/workstaff-home-hero.jpg";
 import CalendarImage from "../../assets/images/organized-calendars.svg";
 import SchedulingImage from "../../assets/images/scheduling-1.webp";
+import TrackingImage from "../../assets/images/time-tracking.webp";
+import DoctorsImage from "../../assets/images/healthcare-scheduling.svg";
+import FeaturesImage from "../../assets/images/Payroll-1.png";
 
 function Home() {
   return (
@@ -95,6 +98,52 @@ function Home() {
             alt="Animated adult with a schedule"
           />
         </div>
+      </div>
+
+      <div className="features">
+        <ul className="cards">
+          <li className="card__item">
+            <img
+              className="card__item-img"
+              src={TrackingImage}
+              alt="Animated human looking at a phone"
+            />
+            <div>
+              <h2 className="card__item-title">On-Demand Shifts</h2>
+              <p className="card__item-subtitle">
+                Single tool for planning and get notified when there's an
+                approval
+              </p>
+            </div>
+          </li>
+          <li className="card__item">
+            <img
+              className="card__item-img"
+              src={DoctorsImage}
+              alt="Two animated doctors planning a schedule"
+            />
+            <div>
+              <h2 className="card__item-title">Booking Shifts but Smoother</h2>
+              <p className="card__item-subtitle">
+                Multiple confirmation options, select shifts depending on your
+                needs
+              </p>
+            </div>
+          </li>
+          <li className="card__item">
+            <img
+              className="card__item-img"
+              src={FeaturesImage}
+              alt="Animated app features"
+            />
+            <div>
+              <h2 className="card__item-title">Scheduling and communication</h2>
+              <p className="card__item-subtitle">
+                Efficiently plan and manage your schedule
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );

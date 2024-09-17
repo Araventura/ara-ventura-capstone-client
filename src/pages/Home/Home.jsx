d740df7a113e313eaa5c504f0b66692982dd25e1
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-16 22:07:43 -0700

    add list items

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.scss";
 import { Link } from "react-router-dom";
 import HeroImage from "../../assets/images/workstaff-home-hero.jpg";
+import CalendarImage from "../../assets/images/organized-calendars.svg";
 
 function Home() {
   return (
@@ -17,7 +18,7 @@ function Home() {
             hours in planning, scheduling, prioritizing communication between
             clinics and dental professionals.
           </h4>
-          <Link className="hero__button">
+          <Link to={"/"} className="hero__button">
             <button>Sing up for free</button>
           </Link>
         </div>
@@ -27,6 +28,32 @@ function Home() {
             src={HeroImage}
             alt="DentaShift hero image"
           />
+        </div>
+      </div>
+      <div className="features">
+        <div className="features__left">
+          <img
+            className="features__left-img"
+            src={CalendarImage}
+            alt="Two animated people chatting"
+          />
+        </div>
+        <div className="features__right">
+          <h2>Easily Publish Job Offers</h2>
+          <ul className="features__right-list">
+            <li className="features__right-item">
+              ✨ Instantly broadcast shifts to potential staff
+            </li>
+            <li className="features__right-item">
+              ✨ Quickly gather interested candidates
+            </li>
+            <li className="features__right-item">
+              ✨ Organize applicants as they apply to shifts
+            </li>
+          </ul>
+          <Link to={"/"} className="hero__button">
+            <button>Sing up for free</button>
+          </Link>
         </div>
       </div>
     </div>

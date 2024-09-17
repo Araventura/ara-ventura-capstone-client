7dd216876b8f5a143c826f334c56fc3ddb982e8d
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-17 12:12:27 -0700

    add links to icons and pointer to links and buttons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 import FacebookIcon from "../../assets/icon/facebook-icon.svg";
 import InstaIcon from "../../assets/icon/instagram-icon.svg";
 import LinkedinIcon from "../../assets/icon/linkedin-icon.svg";
@@ -9,9 +10,23 @@ function Footer() {
     <div className="footer">
       <h4 className="footer__subtitle">Ara Ventura © DentaShift 2024</h4>
       <div className="fotter__icon-wrapper">
-        <img className="footer__icon" src={FacebookIcon} alt="" />
-        <img className="footer__icon" src={InstaIcon} alt="" />
-        <img className="footer__icon" src={LinkedinIcon} alt="" />
+        <Link to="https://www.facebook.com/">
+          <img
+            className="footer__icon"
+            src={FacebookIcon}
+            alt="Facebook Icon"
+          />
+        </Link>
+        <Link to="https://www.instagram.com/">
+          <img className="footer__icon" src={InstaIcon} alt="Instagram Icon" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/ara-ventura/">
+          <img
+            className="footer__icon"
+            src={LinkedinIcon}
+            alt="LinkedIn Icon"
+          />
+        </Link>
       </div>
     </div>
   );

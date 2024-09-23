b13ae85a18b06bd0256c46ce00a42f14c1ea548e
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-22 19:49:16 -0700

    change margin on post component, add useparams, usenavigate, usestate and axios

--- src/components/PostJob/PostJob.jsx
+++ src/components/PostJob/PostJob.jsx
@@ -1,9 +1,54 @@
-import React from "react";
 import "./PostJob.scss";
-import { Link } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import CloseIcon from "../../assets/icon/close.svg";
 
 function PostJob() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    officeId: params.id,
+    jobTitle: "",
+    description: "",
+    dateStart: "",
+    dateEnd: "",
+    payMin: "",
+    payMax: "",
+    status: "Open",
+  });
+
+  const handleInputChange = (e) => {
+    const nextFormData = {
+      ...formData,
+      [e.target.name]: e.target.value,
+    };
+    setFormData(nextFormData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const sendData = await axios.post("http://localhost:8080/jobs", formData);
+      if (sendData.status === 201) {
+        setFormData({
+          officeId: params.id,
+          jobTitle: "",
+          description: "",
+          dateStart: "",
+          dateEnd: "",
+          payMin: "",
+          payMax: "",
+          status: "Open",
+        });
+        navigate("/offices/4");
+      }
+    } catch (e) {
+      console.error("There was an error adding the item", e);
+    }
+  };
   return (
     <div className="post">
       <div className="post__wrapper">
@@ -17,50 +62,69 @@ function PostJob() {
           </Link>
         </div>
         <div className="post__bottom">
-          <form className="form" action="">
+          <form
+            onChange={(e) => handleInputChange(e)}
+            className="form"
+            action=""
+            onSubmit={(e) => handleSubmit(e)}
+          >
             <label className="form__label" htmlFor="">
               What type of dental professional are you looking for?:
             </label>
             <input
               className="form__input"
-              type="text"
+              name="jobTitle"
               placeholder="Type here"
+              required
             />
             <label className="form__label" htmlFor="">
               The description of this job
             </label>
             <textarea
               className="form__input"
-              type="text"
+              name="description"
               placeholder="Add description of job"
+              required
             ></textarea>
             <label className="form__label" htmlFor="">
-              What dates do you need this service for?
+              What dates do you need this service for? START date
             </label>
             <input
               className="form__input"
-              type="text"
+              name="dateStart"
               placeholder="YYYY-MM-DD"
+              required
+            />
+            <label className="form__label" htmlFor="">
+              What dates do you need this service for? END date
+            </label>
+            <input
+              className="form__input"
+              name="dateEnd"
+              placeholder="YYYY-MM-DD"
+              required
             />
             <label className="form__label" htmlFor="">
               The minimum hourly rate
             </label>
-            <input className="form__input" type="text" placeholder="Minimum" />
+            <input
+              className="form__input"
+              name="payMin"
+              placeholder="Minimum"
+              required
+            />
             <label className="form__label" htmlFor="">
               The maximum hourly rate
             </label>
-            <input className="form__input" type="text" placeholder="Maximum" />
-            <label className="form__label" htmlFor="">
-              The job status
-            </label>
             <input
               className="form__input"
-              type="text"
-              placeholder="Open or Closed"
+              name="payMax"
+              placeholder="Maximum"
+              required
             />
-            <Link className="form__link">
-              <button className="form__button">Post</button>
-            </Link>
+            <button type="submit" className="form__button">
+              Post
+            </button>
           </form>
         </div>
       </div>

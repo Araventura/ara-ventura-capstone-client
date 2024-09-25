0d068e580d057acd1e8d4cd115d2ab6eab92e137
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-24 20:59:52 -0700

    add logic to header to use location to compare text and add a class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,50 @@
 import React from "react";
 import "./Header.scss";
 import Logo from "../../assets/logo/DentaShift.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+
+  // Determine the active route
+  const route =
+    location.pathname === "/"
+      ? "home"
+      : location.pathname.includes("offices")
+      ? "offices"
+      : location.pathname.includes("jobs")
+      ? "jobs"
+      : "";
+
   return (
     <div className="header">
       <div className="header__logo-wrapper">
         <img src={Logo} alt="DentaShift Logo" />
       </div>
       <div className="header__buttons">
-        <Link to={"/"} className="header__button-home">
-          <button> Home</button>
+        <Link
+          to={"/"}
+          className={`header__button-home ${
+            route === "home" ? "active-button" : ""
+          }`}
+        >
+          Home
         </Link>
-        <Link to={"/offices/4"} className="header__button-office">
-          <button>Dental Office</button>
+        <Link
+          to={"/offices/4"}
+          className={`header__button-office ${
+            route === "offices" ? "active-button" : ""
+          }`}
+        >
+          Dental Office
         </Link>
-        <Link to={"/jobs"} className="header__button-professional">
-          <button>Dental Professional</button>
+        <Link
+          to={"/jobs"}
+          className={`header__button-professional ${
+            route === "jobs" ? "active-button" : ""
+          }`}
+        >
+          Dental Professional
         </Link>
       </div>
     </div>
